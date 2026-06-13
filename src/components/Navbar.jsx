7feea42350be7ff267a36e19f57b932cd6e634a1
primovere <p.8izo3@slmail.me>
2026-06-13 23:35:13 +0800

    Add cart icon with quantity badge to Navbar
    Include hover and active interaction states.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router";
+import cartIcon from "../assets/cart-shopping-solid-full.svg";
+import "../styles/Navbar.css";
 
 function Navbar({ cartItemCount }) {
   return (
     <nav className="navbar">
       <Link to="/">Shop</Link>
-      <div>
-        <Link to="cart">Cart</Link>
-        <div>{cartItemCount}</div>
-      </div>
+      <Link to="cart">
+        <div className="cart-icon">
+          <img src={cartIcon} alt="cart icon" />
+          <div className="badge">{cartItemCount}</div>
+        </div>
+      </Link>
     </nav>
   );
 }
