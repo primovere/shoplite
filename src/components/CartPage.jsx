1e40d753c172873c122122c4e64d2f61bf271500
primovere <p.8izo3@slmail.me>
2026-06-17 17:54:03 +0800

    Style order summary layout

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -27,45 +27,47 @@ function CartPage({
   return (
     <>
       <h1>Cart</h1>
-      <div className="cart-items">
-        {cartItemsWithProductData.map((item) => {
-          return (
-            <CartItem
-              key={item.id}
-              id={item.id}
-              image={item.image}
-              title={item.title}
-              price={item.price}
-              quantity={item.quantity}
-              onQuantityChange={onQuantityChange}
-              onRemoveItem={onRemoveItem}
-              onQuantityClick={onQuantityClick}
-              subtotal={item.subtotal}
-            />
-          );
-        })}
-      </div>
-      <div className="order-summary">
-        <h2>Order Summary</h2>
-        <div className="total-products">
-          <p>Total products</p>
-          <p>{cartItemCount}</p>
+      <div className="cart-page">
+        <div className="cart-items">
+          {cartItemsWithProductData.map((item) => {
+            return (
+              <CartItem
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                price={item.price}
+                quantity={item.quantity}
+                onQuantityChange={onQuantityChange}
+                onRemoveItem={onRemoveItem}
+                onQuantityClick={onQuantityClick}
+                subtotal={item.subtotal}
+              />
+            );
+          })}
         </div>
-        <div className="product-total">
-          <p>Products</p>
-          <p>{productTotal.toFixed(2)}</p>
+        <div className="order-summary">
+          <h2>Order Summary</h2>
+          <div className="total-products row">
+            <p>Total products</p>
+            <p>{cartItemCount}</p>
+          </div>
+          <div className="product-total row">
+            <p>Products</p>
+            <p>{productTotal.toFixed(2)}</p>
+          </div>
+          <div className="delivery row">
+            <p>Delivery</p>
+            <p>{delivery}</p>
+          </div>
+          <div className="total row">
+            <p>Total</p>
+            <p>{(productTotal + delivery).toFixed(2)}</p>
+          </div>
+          <button className="checkout-btn" type="button">
+            Continue to checkout
+          </button>
         </div>
-        <div className="delivery">
-          <p>Delivery</p>
-          <p>{delivery}</p>
-        </div>
-        <div className="total">
-          <p>Total</p>
-          <p>{(productTotal + delivery).toFixed(2)}</p>
-        </div>
-        <button className="checkout-btn" type="button">
-          Continue to checkout
-        </button>
       </div>
     </>
   );
