0544e6bb6faae708bbb766b555105e0b0b5b6255
primovere <p.8izo3@slmail.me>
2026-06-19 15:35:50 +0800

    Add hero image to ShopPage

--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -1,23 +1,29 @@
 import ProductCard from "./ProductCard.jsx";
 import "../styles/ShopPage.css";
+import heroImage from "../assets/hero-image.png";
 
 function ShopPage({ products, onAddToCartClick }) {
   return (
-    <div className="products">
-      {products.map((product) => {
-        return (
-          <ProductCard
-            key={product.id}
-            imgURL={product.image}
-            title={product.title}
-            price={product.price}
-            onClick={() => {
-              onAddToCartClick(product.id);
-            }}
-          />
-        );
-      })}
-    </div>
+    <>
+      <section className="hero full-bleed">
+        <img src={heroImage} alt="hero image" className="hero-img" />
+      </section>
+      <div className="products">
+        {products.map((product) => {
+          return (
+            <ProductCard
+              key={product.id}
+              imgURL={product.image}
+              title={product.title}
+              price={product.price}
+              onClick={() => {
+                onAddToCartClick(product.id);
+              }}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
 
