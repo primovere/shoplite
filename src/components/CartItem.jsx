3aeabad7e6ff816a2906bab9863cacbda7766b1b
primovere <p.8izo3@slmail.me>
2026-07-03 16:25:36 +0800

    Make CartItem responsive

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -16,11 +16,24 @@ function CartItem({
       <img className="item-img" src={image} alt={title} />
       <div className="right-section">
         <div className="info">
-          <p>{title}</p>
-          <p>{price}/piece</p>
+          <p className="title">{title}</p>
+          <p className="subtotal">
+            <b>${subtotal.toFixed(2)}</b>
+          </p>
+          <p className="price">${price}/piece</p>
         </div>
         <div className="bottom-row">
           <div className="actions">
+            <button
+              className="remove-btn"
+              type="button"
+              onClick={() => onRemoveItem(id)}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                <path d="M232.7 69.9C237.1 56.8 249.3 48 263.1 48L377 48C390.8 48 403 56.8 407.4 69.9L416 96L512 96C529.7 96 544 110.3 544 128C544 145.7 529.7 160 512 160L128 160C110.3 160 96 145.7 96 128C96 110.3 110.3 96 128 96L224 96L232.7 69.9zM128 208L512 208L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 208zM216 272C202.7 272 192 282.7 192 296L192 488C192 501.3 202.7 512 216 512C229.3 512 240 501.3 240 488L240 296C240 282.7 229.3 272 216 272zM320 272C306.7 272 296 282.7 296 296L296 488C296 501.3 306.7 512 320 512C333.3 512 344 501.3 344 488L344 296C344 282.7 333.3 272 320 272zM424 272C410.7 272 400 282.7 400 296L400 488C400 501.3 410.7 512 424 512C437.3 512 448 501.3 448 488L448 296C448 282.7 437.3 272 424 272z" />
+              </svg>
+            </button>
+
             <div className="quantity-control">
               <button
                 className="decrement-btn"
@@ -51,21 +64,7 @@ function CartItem({
                 </svg>
               </button>
             </div>
-
-            <button
-              className="remove-btn"
-              type="button"
-              onClick={() => onRemoveItem(id)}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
-                <path d="M232.7 69.9C237.1 56.8 249.3 48 263.1 48L377 48C390.8 48 403 56.8 407.4 69.9L416 96L512 96C529.7 96 544 110.3 544 128C544 145.7 529.7 160 512 160L128 160C110.3 160 96 145.7 96 128C96 110.3 110.3 96 128 96L224 96L232.7 69.9zM128 208L512 208L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 208zM216 272C202.7 272 192 282.7 192 296L192 488C192 501.3 202.7 512 216 512C229.3 512 240 501.3 240 488L240 296C240 282.7 229.3 272 216 272zM320 272C306.7 272 296 282.7 296 296L296 488C296 501.3 306.7 512 320 512C333.3 512 344 501.3 344 488L344 296C344 282.7 333.3 272 320 272zM424 272C410.7 272 400 282.7 400 296L400 488C400 501.3 410.7 512 424 512C437.3 512 448 501.3 448 488L448 296C448 282.7 437.3 272 424 272z" />
-              </svg>
-            </button>
           </div>
-
-          <p>
-            <b>{subtotal.toFixed(2)}</b>
-          </p>
         </div>
       </div>
     </div>
