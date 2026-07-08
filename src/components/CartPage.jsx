2905e37beb39aae19616536fa7ea1a34805cfc2f
primovere <p.8izo3@slmail.me>
2026-07-08 18:10:08 +0800

    Add cart empty state

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -29,50 +29,62 @@ function CartPage({
   return (
     <>
       <div className="cart-page">
-        <div className="left-section">
-          <h1>Cart</h1>
-          <div className="cart-items">
-            {cartItemsWithProductData.map((item) => {
-              return (
-                <CartItem
-                  key={item.id}
-                  id={item.id}
-                  image={item.image}
-                  title={item.title}
-                  price={item.price}
-                  quantity={item.quantity}
-                  onQuantityChange={onQuantityChange}
-                  onRemoveItem={onRemoveItem}
-                  onQuantityClick={onQuantityClick}
-                  subtotal={item.subtotal}
-                />
-              );
-            })}
+        {cartItemsWithProductData.length > 0 ? (
+          <div className="left-section">
+            <h1>Cart</h1>
+            <div className="cart-items">
+              {cartItemsWithProductData.map((item) => {
+                return (
+                  <CartItem
+                    key={item.id}
+                    id={item.id}
+                    image={item.image}
+                    title={item.title}
+                    price={item.price}
+                    quantity={item.quantity}
+                    onQuantityChange={onQuantityChange}
+                    onRemoveItem={onRemoveItem}
+                    onQuantityClick={onQuantityClick}
+                    subtotal={item.subtotal}
+                  />
+                );
+              })}
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="empty-cart">
+            <h1>Your shopping cart is empty.</h1>
+            <p>
+              When you add products to your shopping cart, they will appear
+              here.
+            </p>
+          </div>
+        )}
 
-        <div className="order-summary">
-          <h2>Order Summary</h2>
-          <div className="total-products row">
-            <p>Total products</p>
-            <p>{cartItemCount}</p>
+        {cartItemsWithProductData.length > 0 && (
+          <div className="order-summary">
+            <h2>Order Summary</h2>
+            <div className="total-products row">
+              <p>Total products</p>
+              <p>{cartItemCount}</p>
+            </div>
+            <div className="product-total row">
+              <p>Products</p>
+              <p>{productTotal.toFixed(2)}</p>
+            </div>
+            <div className="delivery row">
+              <p>Delivery</p>
+              <p>{delivery}</p>
+            </div>
+            <div className="total row">
+              <p>Total</p>
+              <p>{(productTotal + delivery).toFixed(2)}</p>
+            </div>
+            <button className="checkout-btn" type="button">
+              Continue to checkout
+            </button>
           </div>
-          <div className="product-total row">
-            <p>Products</p>
-            <p>{productTotal.toFixed(2)}</p>
-          </div>
-          <div className="delivery row">
-            <p>Delivery</p>
-            <p>{delivery}</p>
-          </div>
-          <div className="total row">
-            <p>Total</p>
-            <p>{(productTotal + delivery).toFixed(2)}</p>
-          </div>
-          <button className="checkout-btn" type="button">
-            Continue to checkout
-          </button>
-        </div>
+        )}
       </div>
     </>
   );
